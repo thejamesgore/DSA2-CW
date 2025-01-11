e99bd7aeccacc5801f5390ae67c32e4f12d0053a
James Gore <83005220+thejamesgore@users.noreply.github.com>
2025-01-11 13:06:58 +0000

    Fix: runtime errors with backtracking and vegas imports

--- Task 1/chessboard.py
+++ Task 1/chessboard.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as mp
 def show_chessboard(moves):
     # we use numpy to create the dimensions of the board with an array of arrays 8x8 and then use slice to make the rows and columns
     chessboard = np.zeros((8, 8))
+    print(chessboard)
     chessboard[::2, ::2] = 1  # we assign the value of 1 to every over element of our array starting at index 0
     chessboard[1::2, 1::2] = 1 # We do the same starting at index 1 on the lines filling the rest of the board
 
